7ea6038fd33821de30d8506723ad5df34557b475
mdLn1 <madalinc.preda@gmail.com>
2020-04-07 22:58:45 +0100

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+class StateComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  handleClick2 = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.handleClick2}> Please Click me</button>
+      </div>
+    );
+  }
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StateComponent />
+      <StateComponent />
+    </div>
+  );
 }
 
 export default App;
